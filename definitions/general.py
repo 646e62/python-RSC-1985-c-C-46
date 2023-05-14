18d6d602b21c6cea05bd62888a21399cf63e1df2
Daniel Nathan Booy <danielnathanbooy@pop-os.localdomain>
2023-05-14 14:05:22 -0600

    Auto saved by Logseq

--- definitions/general.py
+++ definitions/general.py
@@ -1,9 +1,14 @@
 '''
-
+This file contains key-value pairs for the general definition section in the Criminal Code.
+Values are designed to hold the following information:
+* Definition list
+  * [-1] — operator (if applicable)
+  * [:-1] — definition
+* French key equivalent
 '''
 
 general_definitions = {
     "Act": [["An Act of Parliament", "An Act of the legislature of the former Province of Canada", "An Act of the legislature of a province", "An Act or ordinance of the legislature of a province, territory or place in force at the time that province, territory or place became a province of Canada.", "and"], "Loi"],
-    "appearance notice": ["A notice in Form 9 issued by a peace officer", "citation à comparaître"],
-    "associated personell": ""
+    "appearance notice": [["A notice in Form 9 issued by a peace officer", "N/A"], "citation à comparaître"],
+    "associated personell": []
 }
