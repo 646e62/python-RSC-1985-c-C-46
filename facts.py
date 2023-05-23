a364b990ccc65ff58417d8091a71b9178594f2e3
Daniel Nathan Booy <danielnathanbooy@pop-os.localdomain>
2023-05-22 21:54:48 -0600

    Defendant class and input correction

--- facts.py
+++ facts.py
@@ -1,4 +1,46 @@
-# Facts
+# Defendant role: principal, party, accessory after the fact, attempt, conspiracy
+
+def establish_party_liability():
+    """
+    Qualifies party liability by type. Presumes knowledge of the different types
+    of party liability.
+    """
+    print("Party liability type (aider, abettor, common intention)? ")
+
+    party_liability_type = input("Type: ")
+    
+    party_liability_types = ["aider", "abettor", "common intention"]
+
+    if party_liability_type.lower() not in party_liability_types:
+        print("Invalid party liability type.")
+        return establish_party_liability()
+    else:
+        return party_liability_type.lower()
+
+def establish_liability():
+    """
+    Determines whether the defendant is liable for the offence as a principal,
+    party, accessory after the fact, attempt, or conspiracy. Or not at all. The
+    function should return as soon as one role is established. 
+    """
+
+    print("What role did the defendant play in the offence? (principal, party, accessory after the fact, attempt, conspiracy, none)")
+    role = input("Role: ")
+
+    roles = ["principal", "party", "accessory after the fact", "attempt", "conspiracy", "none"]
+
+    if role.lower() not in roles:
+        print("Invalid role.")
+        return establish_liability()
+    elif role.lower() == "party":
+        return establish_party_liability()
+    else:
+        return role.lower()
+
+# High treason
+
+# Actions
+
 def get_sovereign_actions():
     """
     Ask the user questions related to actions against the sovereign.
@@ -40,6 +82,8 @@ def get_canada_actions():
             actions.append(("canada", question[1]))
 
     return actions
+
+# Facts
 
 def high_treason_facts(victim_name):
     """
