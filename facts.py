2bd3f69d4b57abf956815b4b93807f00751e6b68
Daniel Booy <deinonychai@gmail.com>
2023-05-27 15:53:32 +0000

    Rules constants refactored into a dictionary

--- facts.py
+++ facts.py
@@ -1,5 +1,11 @@
-# Defendant role: principal, party, accessory after the fact, attempt, conspiracy
+'''
+Generates a factual matrix that can be used to determine whether the facts of
+the case make out a particular offence.
+'''
 
+import rules_constants
+
+# Defendant role: principal, party, accessory after the fact, attempt, conspiracy
 def establish_party_liability():
     """
     Qualifies party liability by type. Presumes knowledge of the different types
@@ -16,6 +22,7 @@ def establish_party_liability():
         return establish_party_liability()
     else:
         return party_liability_type.lower()
+    
 
 def establish_liability():
     """
@@ -37,54 +44,38 @@ def establish_liability():
     else:
         return role.lower()
 
+
 # High treason
-
-# Actions
-
+## Actions
 def get_sovereign_actions():
     """
     Ask the user questions related to actions against the sovereign.
     """
-    attack_on_sovereign_actions = [
-        ("Did the defendant kill the king?", ["kill"]),
-        ("Did the defendant attempt to kill the king?", ["kill", "attempt"]),
-        ("Did the defendant do bodily harm to the king tending to death?", ["bodily harm", "tending to death"]),
-        ("Did the defendant do bodily harm to the king tending to destruction?", ["bodily harm", "tending to destruction"]),
-        ("Did the defendant maim the king?", ["maim"]),
-        ("Did the defendant wound the king?", ["wound"]),
-        ("Did the defendant imprison the king?", ["imprison"]),
-        ("Did the defendant restrain the king?", ["restrain"])
-    ]
 
     actions = []
-    for question in attack_on_sovereign_actions:
+    for question in rules_constants.HIGH_TREASON_RULES["sovereign"]:
         response = input(question[0] + " (yes/no): ")
         if response.lower() == 'yes':
             actions.append(("sovereign", question[1]))
     
     return actions
 
+
 def get_canada_actions():
     """
     Ask the user questions related to actions against Canada.
     """
-    war_against_canada_actions = [
-        ("Did the defendant levy war against Canada?", ["levy war"]),
-        ("Did the defendant prepare to levy war against Canada?", ["prepare", "levy war"]),
-        ("Did the defendant assist an enemy at war with Canada?", ["assist warring enemy"]),
-        ("Did the defendant assist an armed force hostily engaged with Canadian Forces?", ["assist hostile force"])
-    ]
 
     actions = []
-    for question in war_against_canada_actions:
+    for question in rules_constants.HIGH_TREASON_RULES["canada"]:
         response = input(question[0] + " (yes/no): ")
         if response.lower() == 'yes':
             actions.append(("canada", question[1]))
 
     return actions
 
-# Facts
 
+## Facts
 def high_treason_facts(victim_name):
     """
     Asks the user questions to determine if the facts of the case make out the offence of high treason.
